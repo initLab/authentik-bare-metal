5077429bc2234a56e849d07ae8484c7f1334a808
Vencislav Atanasov <git@6bez10.info>
2023-12-11 22:43:42 +0200

    Update system preparation script

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -yqq # Ensure up-to-date system
-sudo apt install -yqq curl wget git build-essential libncursesw5-dev libssl-dev \
-     libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev pkg-config libffi-dev zlib1g-dev libxmlsec1 libxmlsec1-dev libxmlsec1-openssl libmaxminddb0 # Install build dependencies
+if [ "$(id -u)" -ne 0 ]
+then
+  echo 'Please run as root!' >&2
+  exit 1
+fi
 
-useradd --create-home --user-group --system --shell /bin/bash authentik
+apt update
+
+# Ensure up-to-date system
+apt upgrade -y
+
+# Install build dependencies
+apt install -y curl wget git build-essential libncursesw5-dev libssl-dev \
+  libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev pkg-config \
+  libffi-dev zlib1g-dev libxmlsec1 libxmlsec1-dev libxmlsec1-openssl \
+  libmaxminddb0
+
+if ! id authentik &>/dev/null;
+then
+  useradd --create-home --user-group --system --shell /bin/bash authentik
+fi
